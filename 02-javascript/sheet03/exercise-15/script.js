7c772a8391975ccc3a4cf3ad1e302d91b6982470
Bel <I.ContrerasGuevara@Outlook.es>
2026-03-30 13:43:03 +0200

    feat: s3e15 Add script

---
+++ 02-javascript/sheet03/exercise-15/script.js
@@ -0,0 +1,10 @@
+const perfil = {
+    nombre: "pepito",
+    contacto: {
+        telefono: "+34 535034023",
+        email: "a@a.com"
+    }
+}
+
+console.log(perfil?.nombre, perfil?.contacto?.direccion);
+

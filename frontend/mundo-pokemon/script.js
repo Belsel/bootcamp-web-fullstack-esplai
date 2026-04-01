4b09ef570b34f3e505aea515ec3555603432f430
Bel <I.ContrerasGuevara@Outlook.es>
2026-04-01 12:46:28 +0200

    feat: Add script with pokemon data

---
+++ frontend/mundo-pokemon/script.js
@@ -0,0 +1,66 @@
+const pokemonData = [
+    {
+        id: 1,
+        name: "bulbasaur",
+        types: ["planta", "veneno"],
+        evoFrom: null,
+        description: "Después de nacer, crece alimentándose de las semillas de su lomo."
+    },
+    {
+        id: 2,
+        name: "ivysaur",
+        types: ["planta", "veneno"],
+        evoFrom: "bulbasaur",
+        description: "Cuando el capullo de su lomo se hincha, desprende un dulce aroma para indicar el florecimiento."
+    },
+    {
+        id: 3,
+        name: "venusaur",
+        types: ["planta", "veneno"],
+        evoFrom: "ivysaur",
+        description: "Después de un día de lluvia, la flor de su lomo tiene un aroma más potente y atrae a otros Pokémon."
+    },
+    {
+        id: 4,
+        name: "charmander",
+        types: ["fuego"],
+        evoFrom: null,
+        description: "La llama de la punta de su cola indica su salud. Si Charmander está sano, arderá con más fuerza."
+    },
+    {
+        id: 5,
+        name: "charmeleon",
+        types: ["fuego"],
+        evoFrom: "charmander",
+        description: "En las montañas rocosas donde viven los Charmeleon, sus colas ardientes brillan como estrellas."
+    },
+    {
+        id: 6,
+        name: "charizard",
+        types: ["fuego", "volador"],
+        evoFrom: "charmeleon",
+        description: "Se dice que el fuego de Charizard arde con más fuerza si ha vivido duras batallas."
+    },
+    {
+        id: 7,
+        name: squirtle,
+        types: ["agua"],
+        evoFrom: null,
+        description: "Se protege con su caparazón y luego contraataca lanzando agua a presión cuando tiene oportunidad."
+    },
+    {
+        id: 8,
+        name: "wartortle",
+        types: ["agua"],
+        evoFrom: "squirtle",
+        description: "Se dice que vive 10.000 años. Su peluda cola es un símbolo de longevidad."
+    },
+    {
+        id: 9,
+        name: "blastoise",
+        types: ["agua"],
+        evoFrom: "wartortle",
+        description: "Los chorros de agua que lanza por los tubos de su caparazón pueden atravesar el acero."
+    }
+]
+

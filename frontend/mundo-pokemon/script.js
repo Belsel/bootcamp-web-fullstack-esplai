1ddf3cc141008aa86d8be5cb4370fffeac012ef0
Bel <I.ContrerasGuevara@Outlook.es>
2026-04-01 13:54:57 +0200

    feat: Modify script with card structure

--- frontend/mundo-pokemon/script.js
+++ frontend/mundo-pokemon/script.js
@@ -43,7 +43,7 @@ const pokemonData = [
     },
     {
         id: 7,
-        name: squirtle,
+        name: "squirtle",
         types: ["agua"],
         evoFrom: null,
         description: "Se protege con su caparazón y luego contraataca lanzando agua a presión cuando tiene oportunidad."
@@ -64,3 +64,91 @@ const pokemonData = [
     }
 ]
 
+const grid = document.getElementById("pokedexgrid");
+
+for (let i = 0; i < pokemonData.length; ++i) {
+    // Article
+    const element = document.createElement("article");
+    element.id = pokemonData[i].id;
+    grid.append(element);
+
+    // Outer div
+    const outerDiv = document.createElement("div");
+    outerDiv.classList.add("pokemon-card");
+    outerDiv.tabIndex = 0;
+    element.append(outerDiv);
+
+    // Figure
+    const figure = document.createElement("figure");
+    outerDiv.append(figure);
+
+    // Img
+    const frontImg = document.createElement("img");
+    frontImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonData[i].id}.gif`;
+    frontImg.alt = pokemonData[i].name;
+    figure.append(frontImg);
+
+    // Fig caption
+    const figCaption = document.createElement("figcaption");
+    figCaption.textContent = `ID / ${pokemonData[i].id}`;
+    figure.append(figCaption);
+
+    // Info div
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("pokemon-info");
+    outerDiv.append(infoDiv);
+
+    // Name title
+    const nameTitle = document.createElement("h2");
+    nameTitle.textContent = pokemonData[i].name;
+    nameTitle.classList.add("pokemon-name")
+    infoDiv.append(nameTitle);
+
+    if (pokemonData[i].types !== null) {
+        // Type container
+        const typeContainer = document.createElement("ul");
+        typeContainer.classList.add("type-container");
+        infoDiv.append(typeContainer);
+
+        // Type
+        pokemonData[i].types.forEach((type) => {
+            const pokeType = document.createElement("li");
+            pokeType.type = type;
+            pokeType.textContent = type;
+            typeContainer.append(pokeType);
+        })
+    }
+
+    if (pokemonData[i].evoFrom !== null) {
+        const evoDiv = document.createElement("div");
+        evoDiv.classList.add("evolution-info");
+        infoDiv.append(evoDiv);
+
+        const evoText = document.createElement("p");
+        evoText.classList.add("evolution-text");
+        evoText.textContent = "Evoluciona de:"
+        evoDiv.append(evoText);
+
+        const evoName = document.createElement("p");
+        evoName.classList.add("evolution-name");
+        evoName.textContent = pokemonData[i].evoFrom;
+        evoDiv.append(evoName);
+
+    }
+
+    // Back info div
+    const backInfoDiv = document.createElement("div");
+    backInfoDiv.classList.add("pokemon-back-info");
+    outerDiv.append(backInfoDiv);
+
+    // Pokedex title
+    const pokedexTitle = document.createElement("h2");
+    pokedexTitle.textContent = "Pokedex";
+    backInfoDiv.append(pokedexTitle);
+
+    // Pokedex description
+    const pokedexDescription = document.createElement("p");
+    pokedexDescription.innerText = pokemonData[i].description;
+    backInfoDiv.append(pokedexDescription);
+
+}

9964761156dff6b0c74adf9a85e78965b4f79a8f
BEL-PC\ICont <cladius95@hotmail.com>
2026-04-06 23:28:11 +0200

    feat: improved backgrounds

--- frontend/mundo-pokemon/utils/pokemon.js
+++ frontend/mundo-pokemon/utils/pokemon.js
@@ -2,16 +2,20 @@ import { TYPE_COLORS, TYPE_COLORS_SECONDARY } from "../types/typeColors.js";
 
 export default function pokemonData(pokemon, specie) {
   const types = pokemon.types.map((type) => type.type.name);
-  const typeColors = types.length === 1
-    ? [TYPE_COLORS[types[0]], TYPE_COLORS_SECONDARY[types[0]]]
-    : types.map((type) => TYPE_COLORS[type]);
+  const typeColors =
+    types.length === 1
+      ? [TYPE_COLORS[types[0]], TYPE_COLORS_SECONDARY[types[0]]]
+      : types.map((type) => TYPE_COLORS[type]);
 
   const formattedPokemon = {
     id: pokemon.id,
     name: pokemon.name,
     types,
     evoFrom: specie.evolves_from_species?.name ?? null,
-    description: specie.flavor_text_entries[0]?.flavor_text,
+    description:
+      specie.flavor_text_entries
+        .find((entry) => entry.language.name === "es")
+        ?.flavor_text.replace(/\f/g, " ") ?? null,
     img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${pokemon.id}.gif`,
     fallbackImg: pokemon.sprites.front_default,
     typeColors,

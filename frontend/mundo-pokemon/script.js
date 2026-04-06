608e90f5fb953190da75cfde609b360041070fd0
BEL-PC\ICont <cladius95@hotmail.com>
2026-04-06 16:24:52 +0200

    feat: Added paralel fetching

--- frontend/mundo-pokemon/script.js
+++ frontend/mundo-pokemon/script.js
@@ -65,138 +65,138 @@ import { getPokemonById, getMaxPokemon } from "./services/getPokemon.js";
 //     }
 // ]
 
-const pokemonData = [];
-const MAX_POKEMON = 386;
-
 const grid = document.getElementById("pokedexgrid");
 
-function loadCards(data, grid) {
-    grid.innerHTML = "";
-    for (let i = 0; i < data.length; ++i) {
-        createCard(data[i], grid);
-    }
-}
+function createCard(data) {
+  // Article
+  const element = document.createElement("article");
+  element.id = `pokemon-${data.id}`;
 
-function createCard(data, grid) {
-    // Article
-    const element = document.createElement("article");
-    element.id = data.id;
-    grid.append(element);
+  // Outer div
+  const outerDiv = document.createElement("div");
+  outerDiv.classList.add("pokemon-card");
+  outerDiv.tabIndex = 0;
+  element.append(outerDiv);
 
-    // Outer div
-    const outerDiv = document.createElement("div");
-    outerDiv.classList.add("pokemon-card");
-    outerDiv.tabIndex = 0;
-    element.append(outerDiv);
+  outerDiv.append(createPokemonFigure(data));
+  outerDiv.append(createInfoDiv(data));
+  outerDiv.append(createBackInfo(data));
 
-    outerDiv.append(createPokemonFigure(data));
-    outerDiv.append(createInfoDiv(data));
-    outerDiv.append(createBackInfo(data));
+  return element;
 }
 
 function createPokemonFigure(data) {
-    // Figure
-    const figure = document.createElement("figure");
+  // Figure
+  const figure = document.createElement("figure");
 
-    // Img
-    const frontImg = document.createElement("img");
-    frontImg.src = data.img;
-    frontImg.alt = data.name;
-    figure.append(frontImg);
+  // Img
+  const frontImg = document.createElement("img");
+  frontImg.src = data.img;
+  frontImg.alt = data.name;
+  figure.append(frontImg);
 
-    // Fig caption
-    const figCaption = document.createElement("figcaption");
-    figCaption.textContent = `ID / ${data.id}`;
-    figure.append(figCaption);
+  // Fig caption
+  const figCaption = document.createElement("figcaption");
+  figCaption.textContent = `ID / ${data.id}`;
+  figure.append(figCaption);
 
-    return figure;
+  return figure;
 }
 
 function createInfoDiv(data) {
-    // Info div
-    const infoDiv = document.createElement("div");
-    infoDiv.classList.add("pokemon-info");
+  // Info div
+  const infoDiv = document.createElement("div");
+  infoDiv.classList.add("pokemon-info");
 
-    // Name title
-    const nameTitle = document.createElement("h2");
-    nameTitle.textContent = data.name;
-    nameTitle.classList.add("pokemon-name")
-    infoDiv.append(nameTitle);
+  // Name title
+  const nameTitle = document.createElement("h2");
+  nameTitle.textContent = data.name;
+  nameTitle.classList.add("pokemon-name");
+  infoDiv.append(nameTitle);
 
-    if (data.types !== null) {
-        infoDiv.append(createTypes(data));
-    }
+  if (data.types !== null) {
+    infoDiv.append(createTypes(data));
+  }
 
-    if (data.evoFrom !== null) {
-        infoDiv.append(createEvo(data));
-    }
+  if (data.evoFrom !== null) {
+    infoDiv.append(createEvo(data));
+  }
 
-    return infoDiv;
+  return infoDiv;
 }
 
 function createTypes(data) {
-    // Type container
-    const typeContainer = document.createElement("ul");
-    typeContainer.classList.add("type-container");
+  // Type container
+  const typeContainer = document.createElement("ul");
+  typeContainer.classList.add("type-container");
 
-    // Type 
-    data.types.forEach((type) => {
-        const pokeType = document.createElement("li");
-        pokeType.type = type;
-        pokeType.textContent = type;
-        typeContainer.append(pokeType);
-    })
+  // Type
+  data.types.forEach((type) => {
+    const pokeType = document.createElement("li");
+    pokeType.textContent = type;
+    pokeType.dataset.type = type;
+    typeContainer.append(pokeType);
+  });
 
-    return typeContainer;
+  return typeContainer;
 }
 
 function createEvo(data) {
-    const evoDiv = document.createElement("div");
-    evoDiv.classList.add("evolution-info");
+  const evoDiv = document.createElement("div");
+  evoDiv.classList.add("evolution-info");
 
-    const evoText = document.createElement("p");
-    evoText.classList.add("evolution-text");
-    evoText.textContent = "Evoluciona de:"
-    evoDiv.append(evoText);
+  const evoText = document.createElement("p");
+  evoText.classList.add("evolution-text");
+  evoText.textContent = "Evoluciona de:";
+  evoDiv.append(evoText);
 
-    const evoName = document.createElement("p");
-    evoName.classList.add("evolution-name");
-    evoName.textContent = data.evoFrom;
-    evoDiv.append(evoName);
+  const evoName = document.createElement("p");
+  evoName.classList.add("evolution-name");
+  evoName.textContent = data.evoFrom;
+  evoDiv.append(evoName);
 
-    return evoDiv;
+  return evoDiv;
 }
 
 function createBackInfo(data) {
-    // Back info div
-    const backInfoDiv = document.createElement("div");
-    backInfoDiv.classList.add("pokemon-back-info");
+  // Back info div
+  const backInfoDiv = document.createElement("div");
+  backInfoDiv.classList.add("pokemon-back-info");
 
-    // Pokedex title
-    const pokedexTitle = document.createElement("h2");
-    pokedexTitle.textContent = "Pokedex";
-    backInfoDiv.append(pokedexTitle);
+  // Pokedex title
+  const pokedexTitle = document.createElement("h2");
+  pokedexTitle.textContent = "Pokedex";
+  backInfoDiv.append(pokedexTitle);
 
-    // Pokedex description
-    const pokedexDescription = document.createElement("p");
-    pokedexDescription.innerText = data.description;
-    backInfoDiv.append(pokedexDescription);
+  // Pokedex description
+  const pokedexDescription = document.createElement("p");
+  pokedexDescription.innerText = data.description;
+  backInfoDiv.append(pokedexDescription);
 
-    return backInfoDiv;
+  return backInfoDiv;
 }
 
-async function loadData(amount) {
-    console.log(`Pibes: ${amount}`);
-    for (let i = 1; i <= amount; ++i) {
-        const pokemon = await getPokemonById(i);
-        pokemonData.push(pokemon);
-        loadCards(pokemonData, grid);
-    }
+async function loadData(amount, grid) {
+  const placeholders = [];
+
+  for (let i = 1; i <= amount; ++i) {
+    const placeholder = document.createElement("div");
+    placeholder.classList.add("pokemon-placeholder");
+    grid.append(placeholder);
+    placeholders.push(placeholder);
+  }
+
+  for (let i = 1; i <= amount; ++i) {
+    getPokemonById(i).then((pokemon) => {
+      const card = createCard(pokemon);
+      placeholders[i - 1].replaceWith(card);
+    });
+  }
 }
 
 async function init() {
-    const amount = await getMaxPokemon();
-    await loadData(amount);
+  const maxPokemon = await getMaxPokemon();
+  await loadData(maxPokemon, grid);
 }
 
 init();

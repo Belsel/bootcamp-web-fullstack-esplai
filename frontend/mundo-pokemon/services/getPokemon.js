91505934f37b2a006fdc7c2afc52444087bd64d1
Bel <I.ContrerasGuevara@Outlook.es>
2026-04-08 09:57:02 +0200

    fix: Added status check for async operations

--- frontend/mundo-pokemon/services/getPokemon.js
+++ frontend/mundo-pokemon/services/getPokemon.js
@@ -5,9 +5,9 @@ export async function getPokemonById(id) {
   const SPECIES_URL = `https://pokeapi.co/api/v2/pokemon-species/${id}/`;
   try {
     const pokeResponse = await fetch(POKEMON_URL);
-    const pokeData = await pokeResponse.json();
-    const specieResponse = await fetch(SPECIES_URL);
-    const specieData = await specieResponse.json();
+    const pokeData = pokeResponse.ok ? await pokeResponse.json() : undefined;
+    const speciesResponse = await fetch(SPECIES_URL);
+    const specieData = speciesResponse.ok ? await speciesResponse.json() : undefined;
     return pokemonData(pokeData, specieData);
   }
   catch (error) {
@@ -19,7 +19,7 @@ export async function getMaxPokemon() {
   const POKEMON_COUNT_URL = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
   try {
     const response = await fetch(POKEMON_COUNT_URL);
-    const data = await response.json();
+    const data = response.ok ? await response.json() : undefined;
     return data.count;
   }
   catch (error) {

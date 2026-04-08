0aba8b69a6c2d0bc93adb5af1bd44f8012e1b7b7
Bel <I.ContrerasGuevara@Outlook.es>
2026-04-08 09:25:13 +0200

    fix: Added error handling to getPokemon

--- frontend/mundo-pokemon/services/getPokemon.js
+++ frontend/mundo-pokemon/services/getPokemon.js
@@ -3,16 +3,26 @@ import pokemonData from "../utils/pokemon.js";
 export async function getPokemonById(id) {
   const POKEMON_URL = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const SPECIES_URL = `https://pokeapi.co/api/v2/pokemon-species/${id}/`;
-  const pokeResponse = await fetch(POKEMON_URL);
-  const pokeData = await pokeResponse.json();
-  const specieResponse = await fetch(SPECIES_URL);
-  const specieData = await specieResponse.json();
-  return pokemonData(pokeData, specieData);
+  try {
+    const pokeResponse = await fetch(POKEMON_URL);
+    const pokeData = await pokeResponse.json();
+    const specieResponse = await fetch(SPECIES_URL);
+    const specieData = await specieResponse.json();
+    return pokemonData(pokeData, specieData);
+  }
+  catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getMaxPokemon() {
   const POKEMON_COUNT_URL = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
-  const response = await fetch(POKEMON_COUNT_URL);
-  const data = await response.json();
-  return data.count;
+  try {
+    const response = await fetch(POKEMON_COUNT_URL);
+    const data = await response.json();
+    return data.count;
+  }
+  catch (error) {
+    console.error(error);
+  }
 }

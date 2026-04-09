5056786ce70e79ec24ad53b2cb78b7f2d694a830
BEL-PC\ICont <cladius95@hotmail.com>
2026-04-09 11:33:20 +0200

    feat: Added filter

--- frontend/mundo-pokemon/services/getPokemon.js
+++ frontend/mundo-pokemon/services/getPokemon.js
@@ -10,9 +10,17 @@ export async function getPokemonById(id) {
   return pokemonData(pokeData, specieData);
 }
 
-export async function getMaxPokemon() {
+export async function getPokemonList() {
   const POKEMON_COUNT_URL = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
   const response = await fetch(POKEMON_COUNT_URL);
   const data = await response.json();
-  return data.count;
+  const pokemonList = {
+    maxPokemon: data.count,
+    entries: data.results.map((pokemon) => {
+      const id = pokemon.url.split("/").at(-2);
+      return { id, name: pokemon.name };
+    }),
+  };
+  console.log(pokemonList);
+  return pokemonList;
 }
